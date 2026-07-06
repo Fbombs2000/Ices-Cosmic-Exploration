bbb857d9314153a65e01980904176715fa66aa31
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2026-07-06 10:50:04 -0400

    Finalizing shop setup
    ### Features
    - Mount buying is now fully automated, there's a safety delay JUST to be safe, but might go back and adjust this a bit in the future...
    - Will auto-turnin without any other plugins as well just to not make dependencies for one thing
    - Will also auto-merge items before attempting to buy the mount booklets / items (this is because if you have multiple stacks, it breaks the system)
    
    ### Ui
    - Added a mount tokens tab, this contains when to go buy booklets for mounts / actually go buy mounts at a users preference.
    - Also contains a nice little table telling you how many of each planetary tokens / booklets / mounts you have currently in your inventory (just nice ui things)
    
    - Configs for when to buy mounts is 900 tokens and 1 mount's worth of items (this is moreso to make sure the user doesn't go buying after ever 2 runs of getting tokens tehe)

--- ICE/Ui/MainUi/SelectableSidebar.cs
+++ ICE/Ui/MainUi/SelectableSidebar.cs
@@ -102,6 +102,8 @@ namespace ICE.Ui.MainUi
 
                     if (ShowDronebitSettings())
                         ImGui_Ice.DrawSelectable_Image(65138, "Dronebit Settings", WindowSelection.DroneShopping);
+
+                    ImGui_Ice.DrawSelectable_Image(26176, "Mount Tokens", WindowSelection.MountShopping);
                 }
                 if (ImGui_Ice.Sidebar_CollaspableHeader("Settings", SidebarTabs.Settings, icon: FontAwesomeIcon.Cog))
                 {
